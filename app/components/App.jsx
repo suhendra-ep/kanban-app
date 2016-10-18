5c14da915c30565684757aa35a71fc792b0eb941
suhendra_ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-10-18 10:58:52 +0800

    Refactor to class components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learning React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Fill periodic table'
-    }
-];
 
-export default () => (
-    <div>
-        <button onClick={() => console.log('Add New Note')}>Add Note</button>
-        <Notes notes={notes}/>
-    </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learning React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Fill periodic table'
+                }
+            ]
+        }
+    }
+
+    render() {
+        const {notes} = this.state;
+        return (
+            <div>
+                <button onClick={() => console.log('Add New Note')}>Add Note</button>
+                <Notes notes={notes} />
+            </div>
+        )
+    }
+}
+
+export default App;
