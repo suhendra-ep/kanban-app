c64321402028c255c50a752a1defaaad6721ba65
suhendra_ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-10-18 10:42:11 +0800

    Refactor the data

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import uuid from 'uuid';
 import Notes from './Notes';
+
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learning React'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Fill periodic table'
+    }
+];
 
 export default () => (
     <div>
         <button onClick={() => console.log('Add New Note')}>Add Note</button>
-        <Notes />
+        <Notes notes={notes}/>
     </div>
 );
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 
-const notes = [
-    {
-        id: uuid.v4(),
-        task: 'Learning React'
-    },
-    {
-        id: uuid.v4(),
-        task: 'Fill periodic table'
-    }
-];
-
-export default () => (
+export default ({notes}) => (
     <ul>
         {notes.map(note => <li key={note.id}>{note.task}</li>)}
     </ul>
