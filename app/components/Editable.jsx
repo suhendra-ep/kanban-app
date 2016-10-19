a28bb07daee7278d610ca1177b4703f4e5cec067
suhendra_ep <IKomangSuhendra.EkaPutra@mitrais.com>
2016-10-19 10:48:52 +0800

    Edit component

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Edit = ({ onEdit = () => { }, value, ...props }) => (
+const Edits = ({ onEdit = () => { }, value, ...props }) => (
     <div>
         <span>edit: {value}</span>
     </div>
@@ -11,5 +11,40 @@ export default ({editing, value, onEdit, ...props}) => {
         return <Edit value={value} onEdit={onEdit} {...props} />
     }
 
-    return <span {...props}>value: {value}</span>;
+    return <span {...props}>{value}</span>;
+}
+
+class Edit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.checkEnter = this.checkEnter.bind(this);
+        this.finishEdit = this.finishEdit.bind(this);
+    }
+
+    render() {
+        const {value, onEdit, ...props} = this.props;
+        return (
+            <input
+                type='text'
+                autoFocus={true}
+                defaultValue={value}
+                onBlur={this.finishEdit}
+                onKeyPress={this.checkEnter}
+                {...props} />
+        );
+    }
+
+    checkEnter(e) {
+        if (e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    }
+
+    finishEdit(e) {
+        const value = e.target.value;
+
+        if (this.props.onEdit) {
+            this.props.onEdit(value);
+        }
+    }
 }
